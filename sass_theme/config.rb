8ed7e24ae98b161fcd49f1b50d10f5f6431714cc
Josue Valencia <josue.valencia@domo.ec>
2017-11-05 09:29:52 +0100

    last update

--- sass_theme/config.rb
+++ sass_theme/config.rb
@@ -1,4 +1,6 @@
 require 'compass/import-once/activate'
+require 'bootstrap-sass'
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -6,11 +8,11 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
-javascripts_dir = "javascripts"
+#javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :compressed
+output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
@@ -22,4 +24,4 @@ output_style = :compressed
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
